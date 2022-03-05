8a139b5c83bf90d602a509376bc37558ad99cda5
ayad imad <imday16@gmail.com>
2022-03-05 20:51:30 +0100

    ajout exercice

--- exercices/exercice.js
+++ exercices/exercice.js
@@ -1,31 +1,31 @@
-
+/*
 // 1 premiere couleur
-let couleur1;
-do{
-    couleur1=window.prompt("Saisir une premire couleur parmi : bleu, jaune ou rouge");
-} 
-while(couleur1 !='bleu' & couleur1!='jaune' && couleur1 !='rouge');
+//let couleur1;
+//do{
+//    couleur1=window.prompt("Saisir une premire couleur parmi : bleu, jaune ou rouge");
+//} 
+//while(couleur1 !='bleu' & couleur1!='jaune' && couleur1 !='rouge');
 
 // 2 deuxieme couleur
-let coulour2;
-do{
-coulour2 = window.prompt("Saisir une deuxieme couleur parmi : bleu, jaune ou rouge");
-}
-while((couleur2!='bleu' && couleur2!='jaune' && couleur2 !='rouge') || couleur1==couleur2)
+//let coulour2;
+//do{
+//coulour2 = window.prompt("Saisir une deuxieme couleur parmi : bleu, jaune ou rouge");
+//}
+//while((couleur2!='bleu' && couleur2!='jaune' && couleur2 !='rouge') || couleur1==couleur2)
 
 // 3 Mélange
-let melange;
-if ((couleur1 == 'jaune' && couleur2=='bleu' ) || (couleur1=='bleu' && couleur2=='jaune')){
-    melange = 'vert'
-}
-else if ((couleur1=='rouge' && couleur2 =='jaune') || (couleur1=='jaune' && couleur2=='rouge')){
-    melange = 'orenge'
-}
-else{
-    melange = 'violet';
-}
+//let melange;
+//if ((couleur1 == 'jaune' && couleur2=='bleu' ) || (couleur1=='bleu' && couleur2=='jaune')){
+//    melange = 'vert'
+//}
+//else if ((couleur1=='rouge' && couleur2 =='jaune') || (couleur1=='jaune' && couleur2=='rouge')){
+ //   melange = 'orenge'
+//}
+//else{
+//    melange = 'violet';
+//}
 
-document.write(`${couleur1} et ${couleur2} donnent du ${malange}`);
+//document.write(`${couleur1} et ${couleur2} donnent du ${malange}`);
 
 
 
@@ -47,3 +47,4 @@ document.write(`${couleur1} et ${couleur2} donnent du ${malange}`);
 //else{
 //    document.write(` le mélange des deux couleurs est le Orange`);
 //}
+*/
---
+++ exercices/tuto.js
@@ -0,0 +1,13 @@
+// les opérateurs
+
+let nb1 = 0;
+nb1 = 0 +10;
+console.log(nb1);
+
+// Les chaines
+let nb = 999;
+let str = `abc ${nb}`; 
+console.log(str);
+
+let chn =  `imad est capable`;
+console.log(chn); 
